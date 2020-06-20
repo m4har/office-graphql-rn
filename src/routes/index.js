cc987fadbc7c6137f3267d34b4a7a015600a913c
mahardicka nurachman <mahardicka404@gmail.com>
2020-06-21 01:01:57 +0700

    initial users and tenant screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,14 @@ import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 // initial screen
 import Login from '../screens/Login';
 import Users from '../screens/Users';
+import Detailuser from '../screens/Users/DetailUser';
+import EditUser from '../screens/Users/EditUser';
+import AddUser from '../screens/Users/AddUser';
 import Tenants from '../screens/Tenants';
-
+import AddTenant from '../screens/Tenants/addTenant';
 //bottomtabs dashboard
 import Dashboard from './BottomTab';
+
 const screenOptions = {
   headerStyle: {
     backgroundColor: '#102f4a',
@@ -25,6 +29,26 @@ const Root = () => {
         <Screen name="dashboard" component={Dashboard} />
         <Screen name="user" component={Users} />
         <Screen name="tenant" component={Tenants} />
+        <Screen
+          name="detailuser"
+          component={Detailuser}
+          options={{title: 'detail user'}}
+        />
+        <Screen
+          name="editUser"
+          component={EditUser}
+          options={{title: 'edit user'}}
+        />
+        <Screen
+          name="addUser"
+          component={AddUser}
+          options={{title: 'add user'}}
+        />
+        <Screen
+          name="addTenant"
+          component={AddTenant}
+          options={{title: 'add tenant'}}
+        />
       </Navigator>
     </NavigationContainer>
   );

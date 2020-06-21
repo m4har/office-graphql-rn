94a6d739e02652d1c7db9ffbf1d17ac279200560
mahardicka nurachman <mahardicka404@gmail.com>
2020-06-21 18:42:29 +0700

    get count

--- src/screens/Home/components/CardInfo.js
+++ src/screens/Home/components/CardInfo.js
@@ -2,16 +2,21 @@ import React, {memo} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {Card, Title, Paragraph} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
+import {useQuery} from '@apollo/react-hooks';
+import {GET_PROFILE} from '../../../graphql/tag';
 
-export const CardInfo = memo(() => {
+export const CardInfo = () => {
+  const {data, loading, error} = useQuery(GET_PROFILE);
   const {navigate} = useNavigation();
-  const toUser = () => navigate('user');
+  const toUser = () => {
+    if (!loading) return navigate('user', {role: data?.profile?.role});
+  };
   const toTenant = () => navigate('tenant');
   return (
     <View style={styles.container}>
       <Card style={styles.card} onPress={toUser}>
         <Card.Content>
-          <Title>10 Users</Title>
+          <Title>{`${data?.profile?.allUsers || 0} Users`}</Title>
           <Paragraph>Active users office</Paragraph>
         </Card.Content>
         <Card.Actions />
@@ -20,14 +25,14 @@ export const CardInfo = memo(() => {
         style={[styles.card, {backgroundColor: '#2ecc71'}]}
         onPress={toTenant}>
         <Card.Content>
-          <Title>10 Tenants</Title>
+          <Title>{`${data?.profile?.myTenant || 0} Tenants`}</Title>
           <Paragraph>Total my tenants</Paragraph>
         </Card.Content>
         <Card.Actions />
       </Card>
     </View>
   );
-});
+};
 
 const styles = StyleSheet.create({
   container: {

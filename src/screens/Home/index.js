883e4112f38ded30e8705aab5804ea9f22480a49
mahardicka nurachman <mahardicka404@gmail.com>
2020-06-21 18:42:23 +0700

    get profile and dashboard info

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {useLazyQuery} from '@apollo/react-hooks';
 import {CardInfo} from './components';
+import {GET_PROFILE} from '../../graphql/tag';
 const Home = () => {
+  const [getProfile, {data, loading, error}] = useLazyQuery(GET_PROFILE);
+  useEffect(() => {
+    getProfile();
+    if (!loading) {
+    }
+  }, [loading]);
   return (
     <View style={styles.container}>
       <CardInfo />

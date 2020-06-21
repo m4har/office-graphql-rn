dd96416dce01986fef56d018b0febbeeabc6aa41
mahardicka nurachman <mahardicka404@gmail.com>
2020-06-21 18:44:50 +0700

    all users screen

--- src/screens/Users/index.js
+++ src/screens/Users/index.js
@@ -1,46 +1,53 @@
 import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, FlatList} from 'react-native';
-import {Card, List, Avatar, FAB} from 'react-native-paper';
-import {useNavigation} from '@react-navigation/native';
+import {Card, List, Avatar, FAB, Text} from 'react-native-paper';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {useQuery} from '@apollo/react-hooks';
+import {GET_PROFILE, ALL_USER} from '../../graphql/tag';
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const Users = () => {
   const [render, setRender] = useState(false);
+  const {profile} = useQuery(GET_PROFILE)?.data;
+  const {loading, data} = useQuery(ALL_USER);
   useEffect(() => {
     setRender(true);
   }, []);
   const {navigate} = useNavigation();
+  const {params} = useRoute();
   const keyExtractor = (_, index) => index.toLocaleString();
-  const RenderItem = () => {
+  const RenderItem = ({item}) => {
     return (
       <>
         <Card
           style={styles.card}
-          onPress={() => navigate('detailuser', {id: '123'})}>
+          onPress={() => navigate('detailuser', {id: item.id})}>
           <List.Item
-            title="Mahardicka"
-            description="Operator"
-            left={props => (
-              <Avatar.Image
-                source={{
-                  uri:
-                    'https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14046.jpg',
-                }}
-              />
-            )}
+            title={item.name}
+            description={item.role}
+            left={props => <Avatar.Image source={{uri: item.photo}} />}
           />
         </Card>
       </>
     );
   };
   if (!render) return <View />;
+  if (loading) return <View />;
   return (
     <View style={styles.container}>
       <FlatList
-        data={arr}
+        data={data.allUser}
+        extraData={data}
         keyExtractor={keyExtractor}
         renderItem={RenderItem}
       />
-      <FAB style={styles.fab} icon="plus" onPress={() => navigate('addUser')} />
+      {/* <Text>{JSON.stringify(data?.allUser)}</Text> */}
+      {profile?.role === 'super admin' && (
+        <FAB
+          style={styles.fab}
+          icon="plus"
+          onPress={() => navigate('addUser')}
+        />
+      )}
     </View>
   );
 };

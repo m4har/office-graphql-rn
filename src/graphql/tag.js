205eddda0ceb84ee50a9614ba0f53ece3eac3702
mahardicka nurachman <mahardicka404@gmail.com>
2020-06-21 18:40:13 +0700

    add tag graphql

---
+++ src/graphql/tag.js
@@ -0,0 +1,64 @@
+import gql from 'graphql-tag';
+
+export const GET_PROFILE = gql`
+  {
+    profile {
+      name
+      photo
+      email
+      role
+      myTenant
+      allUsers
+    }
+  }
+`;
+
+export const ALL_USER = gql`
+  {
+    allUser {
+      id
+      name
+      photo
+      role
+    }
+  }
+`;
+
+export const LOGIN_MUTATE = gql`
+  mutation login($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+export const DETAIL_USER = gql`
+  query user($id: String!) {
+    user(id: $id) {
+      id
+      name
+      photo
+      role
+      email
+    }
+  }
+`;
+
+export const ALL_ROLE = gql`
+  {
+    role {
+      role
+    }
+  }
+`;
+
+export const CREATE_USER = gql`
+  mutation create(
+    $name: String!
+    $email: String!
+    $password: String!
+    $role: String!
+  ) {
+    createUser(name: $name, email: $email, password: $password, role: $role)
+  }
+`;
